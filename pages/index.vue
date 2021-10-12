e18f44f47420d0069e19f5a52df908fb44dfedb4
majunon <fayardjul@gmail.com>
2021-10-12 09:20:11 +0200

    cleaning

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <Tutorial/>
+  <div>
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+    </li>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const posts = await $content("blog").fetch();
+
+    return {
+      posts,
+    };
+  },
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
 </script>
