562d0525152774b457f5ed30dd8711319797cbd0
majunon <fayardjul@gmail.com>
2021-10-12 11:48:15 +0200

    import buefy as a module

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    'nuxt-buefy',
   ],
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
