24d7a23efd342ea1c584e47ba98f264202d2f722
majunon <fayardjul@gmail.com>
2021-10-12 15:06:06 +0200

    no need of next and prev info in blog posts anymore !

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -3,26 +3,26 @@
     <h2 class="subtitle is-2 has-text-info">{{ post.title }}</h2>
     <nuxt-content :document="post" />
     <nav class="pagination is-justify-content-center">
-      <NuxtLink v-if="post.previous" class="pagination-previous" :to="{path: post.previous}">Previous</NuxtLink>
+      <NuxtLink v-if="prev" class="pagination-previous" :to="{ path: prev.slug }">Previous</NuxtLink>
       <NuxtLink class="pagination-previous" to="/">Home</NuxtLink>
-      <NuxtLink v-if="post.next" class="pagination-previous" :to="{path: post.next}">Next</NuxtLink>
+      <NuxtLink v-if="next" class="pagination-previous" :to="{ path: next.slug }">Next</NuxtLink>
     </nav>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData({ $content, params, error }) {
-    let post;
-    try {
-      post = await $content("blog", params.slug).fetch();
-      
-    } catch (e) {
-      error({ message: "Blog Post not found" });
-    }
-
+  async asyncData({ $content, params}) {
+    const post = await $content("blog", params.slug).fetch();
+    const [prev, next] = await $content("blog")
+      .only(['title', 'slug'])
+      .sortBy('title', 'asc')
+      .surround(params.slug)
+      .fetch();     
     return {
       post,
+      prev,
+      next
     };
   },
 };
